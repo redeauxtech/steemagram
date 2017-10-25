14f5b7774133c2557ae5724ccc554dce14b4b1c9
FloatCOIN <floatbeta@gmail.com>
2017-10-25 02:35:44 -0500

    Add files via upload

---
+++ needs/prompt.php
@@ -0,0 +1,13 @@
+<div class="prompt model-shadow">
+  <div class="prompt_top">
+    <span class="prompt_title">Delete post</span>
+    <span><i class="material-icons">clear</i></span>
+  </div>
+  <div class="prompt_middle">
+    <span class="prompt_content">Are you sure? Once you confirm deletion of this post, IT CAN NOT BE UNDONE!</span>
+  </div>
+  <div class="prompt_bottom">
+    <a href="#" class="sec_btn prompt_cancel">Cancel</a>
+    <a href="#" class="pri_btn prompt_done">Delete</a>
+  </div>
+</div>
